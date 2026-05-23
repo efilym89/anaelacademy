f6ea6c05cc64311929e9fe38ce04b55a8d1ce4f0
efilym89 <efilym@mail.ru>
2026-05-23 12:14:44 +0300

    Publish annaelle landing

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuButton = document.querySelector(".menu-toggle");
+const navigation = document.querySelector(".main-nav");
+
+if (menuButton && navigation) {
+  menuButton.addEventListener("click", () => {
+    const isOpen = navigation.classList.toggle("is-open");
+    menuButton.setAttribute("aria-expanded", String(isOpen));
+  });
+
+  navigation.addEventListener("click", (event) => {
+    if (event.target instanceof HTMLAnchorElement) {
+      navigation.classList.remove("is-open");
+      menuButton.setAttribute("aria-expanded", "false");
+    }
+  });
+}
